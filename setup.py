9659bd8ff3df6d19841a7705bceddd21a744db74
Necdet Can Atesman <c.atesman@gmx.net>
2015-11-25 18:17:31 +0100

    Release: 0.2.11

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.init',
-    version='0.2.10',
+    version='0.2.11',
     description='Automatic initialization of The SCORE Framework',
     long_description=README,
     author='strg.at',
