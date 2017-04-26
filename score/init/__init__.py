0806642b53277f77cfc968b08fabe3a6fd58088f
Necdet Can Ateşman <can@strg.at>
2017-04-26 14:01:31 +0200

    Release: 0.5.2

--- score/init/__init__.py
+++ score/init/__init__.py
@@ -42,7 +42,7 @@ from .config import (
 
 from .autoimport import import_from_submodules
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 __all__ = (
     'init', 'init_from_file', 'init_logging_from_file', 'InitializationError',
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.init',
-    version='0.5.1',
+    version='0.5.2',
     description='Automatic initialization of The SCORE Framework',
     long_description=README,
     author='strg.at',
