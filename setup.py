6edcc9ab49214f15c9e26da9ce0c1fd7205d1dad
Necdet Can Ateşman <can@strg.at>
2017-08-28 14:52:38 +0200

    Release: 0.5.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.init',
-    version='0.5.4',
+    version='0.5.5',
     description='Automatic initialization of The SCORE Framework',
     long_description=README,
     author='strg.at',
